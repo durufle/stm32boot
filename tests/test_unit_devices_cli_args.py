1411f2cd192dce5d977a1af1f55968595a7d8eb6
Laurent Woolcap <laurent.woolcap@free.fr>
2024-03-27 15:56:57 +0100

    Add docstring missin

--- tests/test_unit_devices_cli_args.py
+++ tests/test_unit_devices_cli_args.py
@@ -20,18 +20,30 @@ runner = CliRunner()
 
 
 def test_main_no_args():
+    """
+    main no args
+    @return:
+    """
     result = runner.invoke(device_app, [])
     assert result.exit_code != 0
     assert "Error: Missing command." in result.stdout
 
 
 def test_main_bad_command():
+    """
+    main invalid command
+    @return:
+    """
     result = runner.invoke(device_app, ["bad"])
     assert result.exit_code != 0
     assert "Error: No such command 'bad'" in result.stdout
 
 
 def test_main_help_option():
+    """
+    main show help
+    @return:
+    """
     result = runner.invoke(device_app, ["--help"])
     assert result.exit_code == 0
     assert "  Devices management cli" in result.stdout
@@ -45,24 +57,40 @@ def test_main_help_option():
 # ----------------------------------------------------------------------------------------------------------------------
 
 def test_command_add_no_args():
+    """
+    add command no args
+    @return:
+    """
     result = runner.invoke(device_app, ["add"])
     assert result.exit_code != 0
     assert "Error: Missing argument 'FILES...'." in result.stdout
 
 
 def test_command_add_bad_option():
+    """
+    add command bad option
+    @return:
+    """
     result = runner.invoke(device_app, ["add", "--bad"])
     assert result.exit_code != 0
     assert "Error: No such option: --bad" in result.stdout
 
 
 def test_command_add_option_check_no_file_name():
+    """
+    add command option check file
+    @return:
+    """
     result = runner.invoke(device_app, ["add", "none.yml"])
     assert result.exit_code == -1
     assert "[Errno 2] No such file or directory: 'none.yml'" in result.stdout
 
 
 def test_command_add_option_check_desc_empty():
+    """
+    add command option check empty description file
+    @return:
+    """
     file = os.path.join(os.path.dirname(__file__), "data/desc_empty.yml")
     result = runner.invoke(device_app, ["add", f"{file}"])
     assert result.exit_code != 0
@@ -70,6 +98,9 @@ def test_command_add_option_check_desc_empty():
 
 
 def test_command_add_option_check_miss_id():
+    """
+    add command option: check device id missing
+    """
     file = os.path.join(os.path.dirname(__file__), "data/desc_miss_id.yml")
     result = runner.invoke(device_app, ["add", f"{file}"])
     assert result.exit_code == 0
@@ -77,6 +108,10 @@ def test_command_add_option_check_miss_id():
 
 
 def test_command_add_option_check_miss_bad_type():
+    """
+    add command option check bad type
+    @return:
+    """
     file = os.path.join(os.path.dirname(__file__), "data/desc_miss_bad_type.yml")
     result = runner.invoke(device_app, ["add", f"{file}"])
     assert result.exit_code == 0
@@ -84,6 +119,9 @@ def test_command_add_option_check_miss_bad_type():
 
 
 def test_command_add_option_check_name_null():
+    """
+    add command option check no name
+    """
     file = os.path.join(os.path.dirname(__file__), "data/desc_name_null.yml")
     result = runner.invoke(device_app, ["add", f"{file}"])
     assert result.exit_code == 0
@@ -91,6 +129,10 @@ def test_command_add_option_check_name_null():
 
 
 def test_command_add_help_option():
+    """
+    add command show help
+    @return:
+    """
     result = runner.invoke(device_app, ["add", "--help"])
     assert result.exit_code == 0
     assert "  Add device description" in result.stdout
@@ -103,6 +145,10 @@ def test_command_add_help_option():
 # list command parameter test parameters
 # ----------------------------------------------------------------------------------------------------------------------
 def test_command_list_help_option():
+    """
+    list command show help
+    @return:
+    """
     result = runner.invoke(device_app, ["list", "--help"])
     assert result.exit_code == 0
     assert "  List supported devices" in result.stdout
@@ -110,18 +156,30 @@ def test_command_list_help_option():
 
 
 def test_command_list_bad_option():
+    """
+    list command bad option
+    @return:
+    """
     result = runner.invoke(device_app, ["list", "--bad"])
     assert result.exit_code != 0
     assert "Error: No such option: --bad" in result.stdout
 
 
 def test_command_list_no_extra_args():
+    """
+    list command bad extra option
+    @return:
+    """
     result = runner.invoke(device_app, ["list", "bad"])
     assert result.exit_code != 0
     assert "Error: Got unexpected extra argument (bad)" in result.stdout
 
 
 def test_command_list_basic_option():
+    """
+    list command check available id
+    @return:
+    """
     result = runner.invoke(device_app, ["list"])
     assert result.exit_code == 0
     assert "0x415" in result.stdout
